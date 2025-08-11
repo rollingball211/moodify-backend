6de93f020cfd9fb9a145f71912d09dde15d95f3e
rollingball211 <jingoo211@gmail.com>
2025-08-11 17:09:38 +0900

    매칭 컨트롤러

--- src/main/java/com/rollingball211/moodify_backend/controller/MoodMusicMappingController.java
+++ src/main/java/com/rollingball211/moodify_backend/controller/MoodMusicMappingController.java
@@ -1,10 +1,15 @@
 package com.rollingball211.moodify_backend.controller;
 
+import com.rollingball211.moodify_backend.domain.Mood;
+import com.rollingball211.moodify_backend.dto.moodMusicMapping.MoodMusicMappingResponseDTO;
 import com.rollingball211.moodify_backend.repository.MoodRepository;
 import com.rollingball211.moodify_backend.service.MoodMusicMappingService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/mood-music")
@@ -20,6 +25,11 @@ public class MoodMusicMappingController {
 
     //MoodID로 음악 가져오기
     //@GetMapping("/{moodId}")
+    public List<MoodMusicMappingResponseDTO> getMuisicByMood(@PathVariable Long moodId){
+        Mood mood = moodRepository.findById(moodId)
+                .orElseThrow(()-> new RuntimeException(("Mood Not Found")));
+        return moodMusicMappingService.getMusicByMood(mood);
+    }
 }
 
 
